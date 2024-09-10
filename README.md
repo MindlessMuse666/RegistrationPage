64de8173808693cf6ce4c1eacf6cf4af320299d8
Vladislav <mindlessmuse.666@gmail.com>
2024-09-10 20:33:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RegistrationPage
+ A student project that performs user registration, written in the Android Studio IDE in Kotlin.
---
+++ app/src/main/java/com/example/registrationpage/DbHelper.kt
@@ -0,0 +1,36 @@
+package com.example.registrationpage
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DbHelper(val context: Context, val factory: SQLiteDatabase.CursorFactory?) : SQLiteOpenHelper(context, "DataBaseName", factory, 1) {
+
+    // Создание Базы Данных
+    override fun onCreate(database: SQLiteDatabase?) {
+        val query = "CREATE TABLE users (id INT PRIMARY KEY, login TEXT, email TEXT, password TEXT)"
+
+        database!!.execSQL(query)
+    }
+
+    // Удаление и пересоздание Базы Данных
+    override fun onUpgrade(database: SQLiteDatabase?, p1: Int, p2: Int) {
+        database!!.execSQL("DROP TABLE IF EXISTS users")
+        onCreate(database)
+    }
+
+    // Регистрация нового пользователя в Базе Данных
+    fun addUser(user: User) {
+        val values = ContentValues()
+        val database = this.writableDatabase
+
+        values.put("login", user.login)
+        values.put("email", user.email)
+        values.put("password", user.password)
+
+        database.insert("users", null, values)
+        database.close()
+    }
+
+}
